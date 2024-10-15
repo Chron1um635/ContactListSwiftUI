084ccfaa8d28cd0b19caa5ad3c69913a344a4fba
Chron1um635 <chron1um522@gmail.com>
2024-10-15 19:35:21 +0300

    setup model

--- ContactListSwiftUI/Models/Contact.swift
+++ ContactListSwiftUI/Models/Contact.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+struct Contact: Identifiable {
+    let id = UUID()
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    var fullname: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getContacts() -> [Contact] {
+        let data = DataStore.shared
+        let names = data.names.shuffled()
+        let surnames = data.surnames.shuffled()
+        let phones = data.phones.shuffled()
+        let emails = data.emails.shuffled()
+        
+        var contacts: [Contact] = []
+        
+        let minCount = min(
+            names.count,
+            surnames.count,
+            phones.count,
+            emails.count
+        )
+        
+        for index in 0..<minCount {
+            contacts.append(
+                Contact(
+                    name: names[index],
+                    surname: surnames[index],
+                    phone: phones[index],
+                    email: emails[index]
+                )
+            )
+        }
+        
+        return contacts
+    }
+}
