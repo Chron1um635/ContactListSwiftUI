7db3f6055c608c3909ebf5658b6c822fc70152f4
Chron1um635 <chron1um522@gmail.com>
2024-10-15 19:35:05 +0300

    setup data storage

--- ContactListSwiftUI/Services/DataStorage.swift
+++ ContactListSwiftUI/Services/DataStorage.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let names  = [
+        "Alex",
+        "Max",
+        "Sergey",
+        "Vladimir",
+        "Vladislav"
+    ]
+    let surnames = [
+        "Murphy",
+        "Jankin",
+        "Williams",
+        "Black",
+        "Bulter"
+    ]
+    let phones = [
+        "99999",
+        "888888",
+        "77777",
+        "66666",
+        "55555"
+    ]
+    var emails = [
+        "ads@das.ru",
+        "dfd@dasf.com",
+        "fsdf@dadf.ru",
+        "agdf@fdfg.com",
+        "fsmof@sfdg.com"
+    ]
+    
+    private init() {}
+}
